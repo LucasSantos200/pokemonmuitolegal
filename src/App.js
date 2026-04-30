1a8a669995e6526458ab9cd661f90a59e8700e16
Lucas Santos <lucas_santos200@estudante.sesisenai.org.br>
2026-04-30 14:37:12 -0300

    adicionei tudo la que ele pediu ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from  'axios';
 import './App.css';
 
 function App() {
+
+  const [pokemons, setPokemons] = useState({}); 
+  const [loading, setLoading] = useState(true); 
+  const [error, setError] = useState(false); 
+
+  useEffect(() => {
+    const getData = async () => {
+      try {const res = await
+        
+        axios.get('https://pokeapi.co/api/v2/pokemon/Bulbasaur');
+        setPokemons(res.data);
+        console.log('Success:', res.data);
+        setLoading(false);
+      }
+      catch (err) {
+        console.error("Error ao carregar API", err); 
+        setLoading(false)
+        setError(true)
+      }
+    };
+    getData();
+  }, {})
+
+
+
+
+
+  if (loading) return <div className='loader'>
+    Carregando Vagabundex
+    </div>;
+ 
+ if (error) return <div className='error'>
+  ocorreu um erro inesperado
+  </div>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cartinha do Pokémon </h1>
+    <div className='pokemon-container'>
+        <div className='pokemon-card'>
+            <h3 className='palavras'>{pokemons.name}</h3>
+            <img src={pokemons.sprites.front_shiny} alt={pokemons.name} />
+            <h2 className='tipo-pokemon1'>{pokemons.types[0].type.name}</h2>
+            
+            <h3 className='palavras'>{
+            pokemons.stats[0].stat.name + ' ' + 
+            pokemons.stats[0].base_stat + ' ' +  pokemons.stats[1].stat.name + ' ' +
+              pokemons.stats[0].base_stat
+              }</h3>
+        </div>
+    </div>   
     </div>
   );
 }
 
 export default App;
+
+
+
