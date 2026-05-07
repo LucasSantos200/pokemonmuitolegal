fa6a9fe158f19024eb3651202279568dde565b4b
Lucas Santos <lucas_santos200@estudante.sesisenai.org.br>
2026-05-07 15:23:15 -0300

    adicionei imagens e uns detalhezinhos
    os botões estão com imagens de pokébolas e quando você passa o mouse em cima ele da uma flutuada, para deixar a estética mais bonitinha

--- src/treinador.js
+++ src/treinador.js
@@ -5,7 +5,7 @@ import './treinador.css';
 
 function Treinador(){
     return(
-        <div className="Treinador">
+        <div className="treinador">
               <h1>Treinador</h1>
               <h2> Avatar</h2>
             <img className='perfil' src="https://static.wikia.nocookie.net/plantas-vs-zumbis-brasil/images/c/cc/Zumbi_Amarrado.png/revision/latest?cb=20220224111357&path-prefix=pt-br" alt="foto linda"/>
@@ -14,7 +14,10 @@ function Treinador(){
                 <h3>Pseyas</h3>
                 <h3>1 por enquanto que eu não ganhei o outro ainda</h3>
                 <h3>0 bagdges</h3>
-                <button></button>
+              
+                        <button className="pokebolas"/>                                 
+                        <button className="pokebolas"/>                                 
+                        <button className="pokebolas"/>                                 
                 </div>
     )
 }
