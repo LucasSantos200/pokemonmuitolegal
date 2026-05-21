82815832a789665790f3e3901b163a081a5db7aa
Lucas Santos <lucas_santos200@estudante.sesisenai.org.br>
2026-05-21 15:56:54 -0300

    s

--- src/pages/pokemon1/pokemon.js
+++ src/pages/pokemon1/pokemon.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './pokemon1.css';
-import pokemonimg from '../../assets/Bulbasauro_irado.png';
 import Gancholegalui from '../../hooks/gancholegalui';
 import { useParams } from "react-router-dom";
 
@@ -13,25 +12,23 @@ function PokemonApp() {
   
   if (loading) return <div className='loader'>Carregando Pokémon...</div>;
   if (error || !pokemons) return <div className='error'>Rolou um erro aqui, calma.</div>;
-
   return (
     <div className="App">
       <h1>Cartinha do Pokémon</h1>
-      <div className='pokemon-container'>
-        <div className={`pokemon-card ${pokemons.types[0].type.name}`}>
+      <div className='pokemon-container '>
+        <div className={`pokemon-carta ${pokemons.types[0].type.name}`}>
           <h3 className={`nome-pkm ${pokemons.types[0].type.name}`}>{pokemons.name}</h3>
           <h2 className={`tipo-pokemon1 ${pokemons.types[0].type.name}`}>{pokemons.types[0].type.name}</h2>
-          <img className='sprite' src={pokemonimg} alt={pokemons.name} />
-          
+          <img className='sprite' src={pokemons.sprites.front_default} alt={pokemons.name} />
           <h3 className={`palavras ${pokemons.types[0].type.name}`}>
             {pokemons.stats[0].stat.name}: {pokemons.stats[0].base_stat}
           </h3>
           <h3 className={`palavras ${pokemons.types[0].type.name}`}>
             {pokemons.stats[1].stat.name}: {pokemons.stats[1].base_stat}
           </h3>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
--- src/pages/treinador/treinador.js
+++ src/pages/treinador/treinador.js
@@ -3,14 +3,14 @@ import { Route, BrowserRouter, Routes, Link } from "react-router-dom";
 import pokemon from "../pokemon1/pokemon";
 import './treinador.css';
 import Botaolegal from "../../components/botaobacana";
-
+import pfp from "../../assets/molierlindaaiaiaiaiaiamburgao.png"
 
 function Treinador(){
     return(
         <div className="Treinador">
               <h1>Treinador</h1>
               <h2> Avatar</h2>
-            <img className='perfil' src="https://static.wikia.nocookie.net/plantas-vs-zumbis-brasil/images/c/cc/Zumbi_Amarrado.png/revision/latest?cb=20220224111357&path-prefix=pt-br" alt="foto linda"/>
+            <img className='perfil' src={pfp} alt="foto linda"/>
                 <h3>Lucas Leandro dos Santos</h3>
                 <h3>16 naos</h3>
                 <h3>Pseyas</h3>
@@ -19,9 +19,9 @@ function Treinador(){
                
                        <Link to="/pokemon/bulbasaur"><Botaolegal></Botaolegal></Link>                             
                        <Link to="/pokemon/magikarp"><Botaolegal></Botaolegal></Link>
-                       <Link to="/pokemon3"><Botaolegal></Botaolegal></Link>
-                       <Link to="/pokemon4"><Botaolegal></Botaolegal></Link>
-                       <Link to="/pokemon5"><Botaolegal></Botaolegal></Link>
+                       <Link to="/pokemon/ponyta"><Botaolegal></Botaolegal></Link>
+                       <Link to="/pokemon/pikachu"><Botaolegal></Botaolegal></Link>
+                       <Link to="/pokemon/"><Botaolegal></Botaolegal></Link>
                        <Link to="/pokemon6"><Botaolegal></Botaolegal></Link> 
                                                          
                 </div>
